0ddd97682cc3264714811692d83b6c6ec0d86734
Marco Antônio <marco.aq7@gmail.com>
2024-10-22 18:06:24 -0300

    update

--- assets/use-quasar.b6d71e7b.js
+++
@@ -1,1 +0,0 @@
-import{i as a,ab as r}from"./index.9b0c46b8.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar.b7708065.js
@@ -0,0 +1,1 @@
+import{i as a,ab as r}from"./index.26ebb66f.js";function u(){return a(r)}export{u};
