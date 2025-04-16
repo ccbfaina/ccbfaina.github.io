a68020888ac89817dabace23be8683fc8c71e56d
Marco Antônio <marco.aq7@gmail.com>
2025-04-16 17:02:01 -0300

    Atualizado Quasar 1.16.4

--- assets/gtag.1f5ab3ed.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.e00bdcb8.js";var p=a(({app:o})=>{});export{p as default};
---
+++ assets/use-quasar-Bu_jUZKJ.js
@@ -0,0 +1,1 @@
+import{i as a,aC as r}from"./index-DX9uuL8x.js";function u(){return a(r)}export{u};
