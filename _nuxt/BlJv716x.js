247a17a51f023c789032ccdc5b8417862541e96d
Marco Antônio <marco.aq7@gmail.com>
2025-12-17 17:22:40 -0300

    up

---
+++ _nuxt/BlJv716x.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./0OxFnqIM.js";import{_ as e,c as _,o as a,b as o,ab as s}from"./D2TRSquu.js";const r={},m={class:"content"};function p(d,i){const t=c,n=s;return a(),_("div",m,[o(t),o(n)])}const x=e(r,[["render",p]]);export{x as default};
